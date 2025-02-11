53b9086900a9af8c489e921a3b4a14f8814d7f74
“lakshmikant” <lakshmikant.lpu@gmail.com>
2025-02-11 21:28:44 +0530

    Changes

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+
+app.get("/top-scores", (req, res) => {
+  res.json({ scores: [100, 95, 90, 85] });
+});
+
+app.listen(3000, () => console.log("Leaderboard service running on port 3000"));
